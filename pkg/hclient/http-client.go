cfda76d450d0bd0f4d89a3c0b32a49422766116a
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2023-02-11 13:10:01 -0800

    add test label into Makefile

--- pkg/hclient/http-client.go
+++ pkg/hclient/http-client.go
@@ -32,10 +32,13 @@ func Get(uri string, params map[string]string) ([]byte, error) {
 func getQueryString(params map[string]string) string {
 	queryString := ""
 	for key, value := range params {
-		if len(queryString) > 0 {
-			queryString += "&"
+		if len(value) > 0 {
+			if len(queryString) > 0 {
+				queryString += "&"
+			}
+			queryString = fmt.Sprintf("%s%s=%s", queryString, key, value)
 		}
-		queryString = fmt.Sprintf("%s%s=%s", queryString, key, value)
+
 	}
 	return queryString
 }
