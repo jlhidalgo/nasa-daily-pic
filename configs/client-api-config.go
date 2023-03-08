a3344d1ee9e89d35504c2b3a6fb0647231ba8bb2
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2023-03-07 20:28:48 -0800

    document configs package

--- configs/client-api-config.go
+++ configs/client-api-config.go
@@ -1,9 +1,16 @@
+// package configs contains configuration settings that can
+// be used by other packages.
 package configs
 
 const (
+
+	// CLIENT_APOD_URI is the URL of the Nasa Picture of the Day API
 	CLIENT_APOD_URI string = "https://api.nasa.gov/planetary/apod"
 )
 
 var (
+
+	// CLIENT_APOD_PARAMS is a map which contains the parameters that
+	// will be used in the APOD API's request
 	CLIENT_APOD_PARAMS = map[string]string{"api_key": "DEMO_KEY"}
 )
--- configs/server-config.go
+++ configs/server-config.go
@@ -1,6 +1,9 @@
 package configs
 
 const (
+	// SERVER_HOSTNAME is the name of the server where the listener will run, i.e. localhost
 	SERVER_HOSTNAME string = ""
-	SERVER_PORT     string = "8081"
+
+	// SERVER_PORT is the port number where the server's listener will run
+	SERVER_PORT string = "8081"
 )
