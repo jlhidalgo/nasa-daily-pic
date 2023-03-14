3b13ae13c5bf663de74a22e79fd4fd0ab7867724
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2023-03-13 19:57:55 -0700

    add doc comments on uri package

--- pkg/uri/uri.go
+++ pkg/uri/uri.go
@@ -1,5 +1,6 @@
-// Package uri implements routines for manipulating URIs.
-// The uri package allows to create a uri by providing both: uri and parameters.
+// Package uri implements routines for validating and generating URLs.
+// The uri package is not really necessary but it was added for educational purposes,
+// especially to learn more about unit testing in Go.
 package uri
 
 import (
@@ -7,11 +8,16 @@ import (
 	"fmt"
 )
 
+// Uri is the representation of the parts of a URL.
+// Path is a simplified version of schema + path, i.e. https://domain.com/.
+// Params are the key-value pairs that will be used in the QueryString.
 type Uri struct {
 	Path   string
 	Params map[string]string
 }
 
+// GetUri returns a concatenation of Path and query string parameters.
+// If Path is empty then returns an error.
 func (u *Uri) GetUri() (string, error) {
 	if len(u.Path) <= 0 {
 		return "", errors.New("failed to get the uri, property Uri is empty")
@@ -26,6 +32,8 @@ func (u *Uri) GetUri() (string, error) {
 	return u.Path, nil
 }
 
+// getQueryString it concatenates key-value pairs obtained from the params map
+// in order to generate the query string to be used in the URL.
 func getQueryString(params map[string]string) string {
 	queryString := ""
 	for key, value := range params {
