2a40e9ab1bcf680cc370705a8101c16697f86675
Juan Luis Hidalgo <juanluis.hidalgo@gmail.com>
2023-03-14 21:46:47 -0700

    add doc comments on rhandler

--- pkg/rhandler/rest-handler.go
+++ pkg/rhandler/rest-handler.go
@@ -1,3 +1,5 @@
+// Package rhandler implements functionality for processing HTTP client requests.
+// In other words this is to consume a REST API.
 package rhandler
 
 import (
@@ -9,17 +11,27 @@ import (
 )
 
 type (
+	// RestHandler implements methods for consuming REST APIs.
+	// HttpClient takes an interface so different clients can be used
+	// interchangeably.
 	RestHandler struct {
 		HttpClient client.IHttpClient
 	}
 )
 
+// NewRestHandler creates a RestHandler and specifies the HTTP client to be used.
+// The net/http client from the Go Standard Library will be usually utilized here,
+// but this current implementation allows to replace it with a mock for
+// testing purposes.
 func NewRestHandler(hClient client.IHttpClient) RestHandler {
 	return RestHandler{
 		HttpClient: hClient,
 	}
 }
 
+// Get implements the GET method for consuming a REST API.
+// Returns the body if the response from the client is successful,
+// otherwise it returns an error.
 func (r RestHandler) Get(uri string) ([]byte, error) {
 	resp, err := r.HttpClient.Get(uri)
 	if err != nil {
@@ -33,6 +45,9 @@ func (r RestHandler) Get(uri string) ([]byte, error) {
 	return getBody(resp)
 }
 
+// getBody retrieves the body from the response by using a reader.
+// Returns the body as a slice of bytes or an error if the body
+// cannot be retrieved.
 func getBody(resp *http.Response) ([]byte, error) {
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
